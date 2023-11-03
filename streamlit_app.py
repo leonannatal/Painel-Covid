9c00124a20f581d56b6755117c66b83aca1f8529
leonannatal <91706948+leonannatal@users.noreply.github.com>
2023-11-03 02:09:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,3 @@ response = requests.request("POST", url, headers=headers, data=payload)
 data = response.json()
 data = pd.json_normalize(data['hits']['hits'])
 data
-
-data['_source.paciente_enumSexoBiologico'] = data['_source.paciente_enumSexoBiologico'].astype(str)
-df_pop = data['_source.paciente_enumSexoBiologico'].value_counts()
-df_pop.plot.pie()
